c1dff1111b6da3726d42ae4ac2e8aa333d4d4283
대성 김 <DaeSeong@jumong.kr>
2018-07-30 15:52:04 +0900

    초기 커밋.

---
+++ CSLecture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
