23531f569f2971c92533aaaaf3cad368832b51a8
대성 김 <DaeSeong@jumong.kr>
2018-07-30 16:20:55 +0900

    HelloWorld 완성

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Hello World!");
+            Console.Write('\n');
+
+            string myString = "Hello World!";
+
+            Console.WriteLine(myString);
+
+            char[] myCharList = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!' };
+
+            Console.WriteLine(myCharList);            
+
+            Console.ReadKey();
+        }
+    }
+}
